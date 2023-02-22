0ec4fc642c56f2c9b1c920e2520fc98634139d2f
Samuel Li Wang <samuliwang@gmail.com>
2023-02-22 15:09:15 +0100

    Theme.js adde to src

--- components/AppBar.jsx
+++ components/AppBar.jsx
@@ -1,11 +1,11 @@
-import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { View, Text, Pressable, StyleSheet, Alert } from 'react-native'
 import Constants from 'expo-constants'
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable>
-      <Text>Repositories</Text>
+      <Pressable onPress={() => Alert.alert('You pressed the App Bar!')}>
+      <Text style={{color: 'white'}}>Repositories</Text>
       </Pressable>
     </View>
   )
@@ -14,6 +14,8 @@ const AppBar = () => {
 const styles = StyleSheet.create({
     container: {
         height: Constants.statusBarHeight
+        , backgroundColor: '#24292e'
+
     }
 })
 export default AppBar
