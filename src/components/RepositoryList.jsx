75d80f0c6c164412cf0989aea00e91df13af8a6e
Samuel Li Wang <samuliwang@gmail.com>
2023-03-02 09:47:43 +0100

    useRepository hook used to refactor RepositoryList component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { View, FlatList, StyleSheet } from "react-native";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import RepositoryItem from "./RepositoryItem";
+import useRepositories from "../hooks/useRepositories";
 
 const styles = StyleSheet.create({
   separator: {
@@ -58,19 +59,7 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState();
-
-  const fetchRepositories = async () => {
-    const response = await fetch("http://192.168.18.102:5001/api/repositories");
-    const json = await response.json();
-
-    console.log(json);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
+  const { repositories } = useRepositories();
 
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
